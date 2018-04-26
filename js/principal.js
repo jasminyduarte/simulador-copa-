d09e514424d3bd9350d0c501e4ad4a9af19884ab
jasminy <jasminyduuarte_@hotmail.om>
2018-04-25 21:56:20 -0400

    versão 1.0

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,4 @@
+//variaveis globais
 
 //Teste ajax
 function apaga() {
@@ -7,70 +8,152 @@ function apaga() {
 
 }
 //select
-$(document).ready(function() {
+$(document).ready(function () {
     $('select').material_select();
 
 });
 
 //pegando dados da tabela
-function Selecionado(){
-    $("select option:selected").each(function() {
+function Selecionado() {
+    $("select option:selected").each(function () {
         var sele = $(this).val();
-        console.log(sele);
-        var Nome_Tabela =  $('#nometabela');
+        var Nome_Tabela = $('#nometabela');
         var Nome_Time_input = $('#nometime').val();
-        var Nome_time = $('.nometimetable');
-        var Nome_time_B = [];
-        var Nome_time_C = [];
-        var Nome_time_D = [];
+        var times = {}; // objeto time
 
-
-    //     Verificando Qual Grupo Foi selecionando
-        if(sele === 'a'){
+        //     Verificando Qual Grupo Foi selecionando
+        if (sele === 'a') {
             Nome_Tabela.html("Grupo A");
-            Nome_time.html(Nome_Time_input);
-        }
-        else if (sele === 'b'){
-            Nome_Tabela.html("Grupo B");
-            for (let index = 0; index < Nome_time_B.length; index++) {
-                nometime.html(Nome_time_B[index]);
+            var table = $('.pai');
+            if (sele !== "a") {
+                table.empty();
             }
+            else {
+                times = {
+                    times: $("#times option:selected").val(),
+                    grupo: Nome_Tabela
+                }
+                table.append(
+                    "<tr>" +
+                    "<td  class= 'center-align white black-text'>" +
+                    times.times +
+                    " </td>" +
+                    "</tr>"
+                );
+                console.log(times)
+            }
+
         }
-        else if (sele === 'c'){
+        else if (sele === 'b') {
+            Nome_Tabela.html("Grupo B");
+            var table = $('.pai');
+
+            if (sele !== "b") {
+                table.empty();
+            }
+            else {
+                table.append(
+                    "<tr>" +
+                    "<td  class= 'center-align white black-text'>" +
+                    times +
+                    " </td>" +
+                    "</tr>"
+                );
+            } // adiciona novo
+        }
+        else if (sele === 'c') {
             Nome_Tabela.html("Grupo C");
-            Nome_time.html(Nome_Time_input);
+            if (sele !== "c") {
+                table.empty();
+            }
+            else {
+                table.append(
+                    "<tr>" +
+                    "<td  class= 'center-align white black-text'>" +
+                    times +
+                    " </td>" +
+                    "</tr>"
+                );
+            } // adiciona novo
         }
-        else if (sele === 'd'){
+        else if (sele === 'd') {
             Nome_Tabela.html("Grupo D");
-            Nome_time.html(Nome_Time_input);
+            var table = $('.pai').append(
+                "<tr>" +
+                "<td  class= 'center-align white black-text'>" +
+                times +
+                " </td>" +
+                "</tr>"
+            );
+        }
+        else if (sele === 'e') {
+            Nome_Tabela.html("Grupo E");
+            var table = $('.pai').append(
+                "<tr>" +
+                "<td  class= 'center-align white black-text'>" +
+                times +
+                " </td>" +
+                "</tr>"
+            );
+        }
+        else if (sele === 'f') {
+            Nome_Tabela.html("Grupo f");
+            var table = $('.pai').append(
+                "<tr>" +
+                "<td  class= 'center-align white black-text'>" +
+                times +
+                " </td>" +
+                "</tr>"
+            );
+        }
+        else if (sele === 'g') {
+            Nome_Tabela.html("Grupo f");
+            var table = $('.pai').append(
+                "<tr>" +
+                "<td  class= 'center-align white black-text'>" +
+                times +
+                " </td>" +
+                "</tr>"
+            );
+        }
+        else if (sele === 'h') {
+            Nome_Tabela.html("Grupo H");
+            var table = $('.pai').append(
+                "<tr>" +
+                "<td  class= 'center-align white black-text'>" +
+                times +
+                " </td>" +
+                "</tr>"
+            );
         }
     });
 }
 
 //iniciando carouesl
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel').carousel();
     $(".button-collapse").sideNav({
         menuWidth: 300, // Default is 300
         edge: 'left', // Choose the horizontal origin
         closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true, // Choose whether you can drag to open on touch screens,
-        onOpen: function(el) { },
-      onClose: function(el) {}, // A function to be called when sideNav is closed
+        onOpen: function (el) { },
+        onClose: function (el) { }, // A function to be called when sideNav is closed
     });
 });
 
 
 
 //Pegando o valor do input e colocando na tabela
-$('input').keyup(function(){
+$('input').keyup(function () {
     //pegando o atual input
     var gol = $(this).val();
-   console.log(gol)
+    console.log(gol)
 
     //pegando referencia da tabela
-    var vitoria_table = $('.vitoria'); 
+    var vitoria_table = $('.vitoria');
     var jogada_table = $('.jogada');
     var ponto_table = $('.ponto');
+     ponto_table.html(gol);
 
 });
